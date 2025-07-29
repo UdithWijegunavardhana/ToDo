cf0b453844841649393f523d66a9133d71ab2b5a
Udith Wijegunavardhana <udith.irng97@gmail.com>
2025-07-29 12:18:00 +0530

    Core: basic configs

---
+++ app/theme/Colors.js
@@ -0,0 +1,35 @@
+const colors = {
+  // Backgrounds
+  white: '#FFFFFF',
+  black: '#000000',
+  background: '#F9F9F9',
+
+  // Text
+  textPrimary: '#000000',
+  textSecondary: '#666666',
+  error: '#FF4C4C',
+  placeholder: '#999999',
+
+  // Primary Action
+  primary: '#2ECC71',
+  primaryDark: '#27AE60',
+  primaryLight: '#A5F1C3',
+
+  // Input
+  inputBackground: '#f2f2f2',
+  inputBorder: '#E0E0E0',
+
+  // Icons
+  starFilled: '#F4C430',
+  starEmpty: '#CCCCCC',
+  border: '#EDEDED',
+
+  // Button
+  buttonText: '#FFFFFF',
+  disabled: '#CCCCCC',
+
+  // Badge
+  badge: '#FF3B30',
+};
+
+export default colors;
